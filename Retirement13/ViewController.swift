d3f817638aaea3f8ccbc81267ae0f95241291df0
WeiTsungCheng <sanctuary85c@gmail.com>
2021-08-10 10:02:49 +0800

    Initial Commit

---
+++ Retirement13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Retirement13
+//
+//  Created by WEI-TSUNG CHENG on 2021/8/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
