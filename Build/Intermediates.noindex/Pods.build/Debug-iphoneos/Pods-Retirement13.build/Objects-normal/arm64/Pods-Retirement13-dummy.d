17f57e47e24621319f81ad81656af8b80605abe6
WeiTsungCheng <sanctuary85c@gmail.com>
2021-09-07 01:22:55 +0800

    feature3-2

--- Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-Retirement13.build/Objects-normal/arm64/Pods-Retirement13-dummy.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/wei-tsung-cheng/Desktop/CI-CD-Course/Retirement13/Pods/Target\ Support\ Files/Pods-Retirement13/Pods-Retirement13-dummy.m \
-  /Users/wei-tsung-cheng/Desktop/CI-CD-Course/Retirement13/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-Retirement13.build/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/dyld.modulemap
--- Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-Retirement13.build/Objects-normal/arm64/Pods_Retirement13_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/wei-tsung-cheng/Desktop/CI-CD-Course/Retirement13/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-Retirement13.build/DerivedSources/Pods_Retirement13_vers.c
